be92f2db3612b2f21b7a9d9b20f50c747418c0db
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-03-18 10:48:10 +0530

    add btn

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 let input = document.querySelector("input");
 let taskList = document.getElementById("tasks");
+let btn = document.querySelector("button");
 
-input.addEventListener("keydown", function (e){
-    if(e.code=="Enter"||e.code=="NumpadEnter"){
-        if(e.target.value==""){
-            alert("Task cannot be empty!");
-            return;
-        }
-        
-        // The task got from the input is added as <li>
-        let task = document.createElement("li");
-        task.innerText=e.target.value;
-        task.classList.add("lis");
+// Add task on clicking the button
+btn.addEventListener("click", addTask);
 
-        // For the task !done the checkbox is created
-        let check= document.createElement("input");    
-        check.type="checkbox";
-        check.classList.add("checkbox");
+// Add task on pressing Enter key
+input.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        addTask();
+    }  
+});
 
-        // For deletion of task a delete icon
-        let span = document.createElement("span");
-        span.classList.add("del");
-
-        // And at last all these added to a <ul> as a child element <li>
-        task.prepend(check);
-        task.append(span);
-        taskList.prepend(task);
-
-        // Clear input field
-        e.target.value="";
+// Function to add task
+function addTask(){
+    if(input.value==""){
+        alert("Task cannot be empty!");
+        return;
     }
-})
+    console.log(input.value);
+    // The task got from the input is added as <li>
+    let task = document.createElement("li");
+    task.innerText=input.value;
+    task.classList.add("lis");
+
+    // For the task !done the checkbox is created
+    let check= document.createElement("input");    
+    check.type="checkbox";
+    check.classList.add("checkbox");
+
+    // For deletion of task a delete icon
+    let span = document.createElement("span");
+    span.classList.add("del");
+
+    // And at last all these added to a <ul> as a child element <li>
+    task.prepend(check);
+    task.append(span);
+    taskList.prepend(task);
+
+    // Clear input field
+    input.value="";
+}
 
 // Function to remove tasks
 document.body.addEventListener("click", function (e) {
